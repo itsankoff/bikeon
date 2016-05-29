26f6ccc561601c677fd88afcb4db7cf52ff58825
itsankoff <itsankoff@gmail.com>
2016-05-29 18:19:54 +0300

    * Implement communication with serveR

--- bike_guard/lock.py
+++ bike_guard/lock.py
@@ -1,22 +1,27 @@
-import RPi.GPIO as GPIO
-import time
+# import RPi.GPIO as GPIO
+# import time
+# 
+# 
+# GPIO.setmode(GPIO.BCM)
+# GPIO.setup(11, GPIO.OUT)
+# GPIO.setup(9, GPIO.IN)
+# GPIO.output(11, GPIO.LOW)
 
 
-GPIO.setmode(GPIO.BCM)
+def lock():
+    return True
 
-GPIO.setup(11, GPIO.OUT)
-GPIO.setup(9, GPIO.IN)
+def unlock():
+    return True
 
-GPIO.output(11, GPIO.LOW)
-
-while True:
-	if GPIO.input(9) == True:
-		print("BEIBE BEIBE")
-		
-		GPIO.output(11, GPIO.HIGH)
-		time.sleep(5)
-
-		GPIO.cleanup()
-
-		break
-
+# while True:
+# 	if GPIO.input(9) == True:
+# 		print("BEIBE BEIBE")
+# 		
+# 		GPIO.output(11, GPIO.HIGH)
+# 		time.sleep(5)
+# 
+# 		GPIO.cleanup()
+# 
+# 		break
+# 
--- bike_guard/station.py
+++ bike_guard/station.py
@@ -2,33 +2,92 @@
 import websocket
 import thread
 import time
+import lock
+import json
 
-def on_message(ws, message):
-    print message
+# in seconds
+ALIVE_INTERVAL = 5
+SERVER_IP = '185.14.184.35'
+# SERVER_IP = '0.0.0.0'
+DEVICE_ID = '491d8a72-24ea-11e6-b67b-9e71128cae77'
+
+
+def on_message(ws, data):
+    try:
+        message = json.loads(data)
+        if message.type and message.device_id and message.device_id == DEVICE_ID:
+            if message.type == 'lock':
+                if lock.lock():
+                    print 'device is lock'
+                    ws.send(json.dumps({
+                        'type': 'lock',
+                        'status': 'success',
+                        'device_id': DEVICE_ID
+                    }))
+                else:
+                    ws.send(json.dumps({
+                        'type': 'lock',
+                        'status': 'fail',
+                        'device_id': DEVICE_ID
+                    }))
+                    print 'fail to lock device'
+            elif message.type == 'unlock':
+                if lock.unlock():
+                    print 'device in unlocked'
+                    ws.send(json.dumps({
+                        'type': 'unlock',
+                        'status': 'success',
+                        'device_id': DEVICE_ID
+                    }))
+                else:
+                    ws.send(json.dumps({
+                        'type': 'unlock',
+                        'status': 'fail',
+                        'device_id': DEVICE_ID
+                    }))
+                    print 'fail to unlock device'
+            else:
+                print 'Unknown command ' + message
+        else:
+            print 'Invalid message' + message
+    except:
+        print 'Message is not json'
+
 
 def on_error(ws, error):
+    ALIVE = False
     print error
 
+
 def on_close(ws):
+    ALIVE = False
     print "### closed ###"
 
+
 def on_open(ws):
-    def run(*args):
-        for i in range(30000):
-            time.sleep(1)
-            ws.send("Hello %d" % i)
-        time.sleep(1)
+    ALIVE = True
+    ws.send(json.dumps({
+        'type': 'auth',
+        'device_id': DEVICE_ID
+    }))
+    def heart_beat(*args):
+        alive_message = json.dumps({
+            'type': 'alive',
+            'device_id': DEVICE_ID
+        })
+        while ALIVE:
+            ws.send(alive_message)
+            time.sleep(ALIVE_INTERVAL)
         ws.close()
-        print "thread terminating..."
-    thread.start_new_thread(run, ())
+        print "stopping device..."
+
+    thread.start_new_thread(heart_beat, ())
 
 
 if __name__ == "__main__":
-    websocket.enableTrace(True)
-    ws = websocket.WebSocketApp("ws://echo.websocket.org/",
+    ws = websocket.WebSocketApp("ws://" + SERVER_IP + ":8888",
                                 on_message = on_message,
                                 on_error = on_error,
                                 on_close = on_close)
     ws.on_open = on_open
-
     ws.run_forever()
