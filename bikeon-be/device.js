378c7f8c95a75e0ba3d985306de559f296e4310f
itsankoff <itsankoff@gmail.com>
2016-05-29 18:20:36 +0300

    * Implement communication with device

--- bikeon-be/device.js
+++ bikeon-be/device.js
@@ -1,23 +1,69 @@
 'use strict';
 
+const ALIVE_INTERVAL = 10
+
 class Device {
-  constructor(id) {
+  constructor(id, connection) {
     this.id = id;
     this.connected = true;
+    this.alive = Date.now();
+    this.connection = connection;
+
+    this.lockSuccess = null;
+    this.lockFail = null;
+    this.unlockSuccess = null;
+    this.unlockFail = null;
   }
 
   lock() {
-    return Promise.resolve();
+    console.log('device lock called', this.id);
+    return new Promise((resolve, reject) => {
+      this.connection.send(JSON.stringify({
+        type: 'lock',
+        device_id: id
+      }));
+
+      this.lockSuccess = resolve;
+      this.lockFail = reject;
+    });
   }
 
   unlock() {
-    return Promise.resolve();
+    console.log('device unlock called', this.id);
+    return new Promise((resolve, reject) => {
+      this.connection.send(JSON.stringify({
+        type: 'unlock',
+        device_id: id
+      }));
+
+      this.unlockSuccess = resolve;
+      this.unlockFail = reject;
+    });
+  }
+
+  lockCallback(status) {
+    if (status) {
+      this.lockSuccess();
+    } else {
+      this.lockFail();
+    }
+
+    this.lockSuccess = null;
+  }
+
+  unlockCallback(status) {
+    if (status) {
+      this.unlockSuccess();
+    } else {
+      this.unlockFail();
+    }
+
+    this.unlockSuccess = null;
   }
 
   get status() {
-    return this.connected;
+    return Date.now() - this.alive < ALIVE_INTERVAL;
   }
 }
-
 
 module.exports = Device;
--- bikeon-be/station.js
+++ bikeon-be/station.js
@@ -10,15 +10,11 @@ class Station {
     // try to connect all the devices
     this.devices = {};
 
-    // mock device
-    var mockId = '491d8a72-24ea-11e6-b67b-9e71128cae77';
-    this.devices[mockId] = new Device(mockId);
-
     // start service to manage devices
     this.deviceServer = new WebSocketChannel();
-    this.deviceServer.onDeviceOpen(this._onDeviceOpen);
-    this.deviceServer.onDeviceMessage(this._onDeviceMessage);
-    this.deviceServer.onDeviceClose(this._onDeviceClose);
+    this.deviceServer.onDeviceOpen(this._onDeviceOpen.bind(this));
+    this.deviceServer.onDeviceMessage(this._onDeviceMessage.bind(this));
+    this.deviceServer.onDeviceClose(this._onDeviceClose.bind(this));
     this.deviceServer.start(config);
   }
 
@@ -30,12 +26,65 @@ class Station {
     console.log('device connected');
   }
 
-  _onDeviceMessage(device, message) {
-    console.log('device message', message);
+  _onDeviceMessage(connection, data) {
+    try {
+      var message = JSON.parse(data);
+    } catch(e) {
+      console.log('device message is not json', data);
+      return;
+    }
+
+    if (message.type && message.device_id) {
+      var deviceId = message.device_id;
+      switch(message.type) {
+      case 'auth':
+        console.log('register device', deviceId);
+        this.devices[deviceId] = new Device(deviceId, connection);
+        break; 
+      case 'alive':
+        console.log('update device alive', deviceId);
+        var device = this.devices[deviceId];
+        if (device) {
+          device.alive = Date.now();
+        }
+        break;
+      case 'lock':
+        console.log('device lock response', this.id);
+        var device = this.devices[deviceId];
+        if (device) {
+          device.lockCallback(true);
+        } else {
+          console.log('no such device', deviceId);
+        }
+        break;
+      case 'unlock':
+        console.log('device unlock response', this.id);
+        var device = this.devices[deviceId];
+        if (device) {
+          device.unlockCallback(true);
+        } else {
+          console.log('no such device', deviceId);
+        }
+        break;
+      }
+    } else {
+      console.log('device invalid message', message);
+    }
   }
 
-  _onDeviceClose(device) {
-    console.log('device closed');
+  _onDeviceClose(connection) {
+    var deviceIds = Object.keys(this.devices);
+    var closedDevice = null;
+    for (var i in deviceIds) {
+      var device = this.devices[deviceIds[i]];
+      if (device.connection === connection) {
+        closedDevice = device;
+        break;
+      }
+    }
+
+    delete this.devices[device.id];
+    console.log('device closed', device.id);
   }
 }
 
