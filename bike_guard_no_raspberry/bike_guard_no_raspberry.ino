aaba6d008ec7093524319ee1ff870e02cb0496ce
Ognyan Angelov <ognyan@chobolabs.com>
2016-05-29 19:18:29 +0300

    Smart lab.

---
+++ bike_guard_no_raspberry/bike_guard_no_raspberry.ino
@@ -0,0 +1,71 @@
+#include <Servo.h>
+
+Servo servo;
+
+const int unlockPin = 4;
+const int soundPin = 5;
+const int servoPin = 6;
+const int vibrationPin = 7;
+const int alarmPin = 8;
+const int ledPin =  13;
+
+int buttonState = 0;
+int isLocked = 0;
+
+int isUnlocked = 0;
+
+int unlockedAngle = 0;
+int lockedAngle = 100;
+
+void setup() {
+  pinMode(vibrationPin, INPUT);
+  pinMode(unlockPin, INPUT);
+  
+  pinMode(alarmPin, OUTPUT);
+  pinMode(soundPin, OUTPUT);
+  pinMode(ledPin, OUTPUT);
+
+  servo.attach(servoPin);
+
+  
+  Serial.begin(9600);
+  servo.write(lockedAngle);
+  delay(15);
+}
+
+void loop() {
+  buttonState = digitalRead(vibrationPin);
+
+  
+  if (buttonState == HIGH) {
+    digitalWrite(ledPin, LOW);
+    // digitalWrite(soundPin, LOW);    
+  } else {
+    digitalWrite(alarmPin, HIGH);
+    digitalWrite(ledPin, HIGH);
+
+    tone(soundPin, 440, 2000);
+  }
+
+  // int isUnlocked = digitalRead(unlockPin);
+  // Serial.println(isUnlocked);
+
+  isUnlocked = buttonState;
+  Serial.println(isUnlocked);
+  
+  if (isUnlocked == HIGH) {
+     //tone(soundPin, 440, 500);
+      servo.write(lockedAngle);
+  //   servo.write(unlockedAngle);
+  } else {
+     //tone(soundPin, 440, 500);
+     servo.write(unlockedAngle);
+      //delay(2000);  
+
+//     servo.write(lockedAngle);
+  }
+
+  delay(15);
+    digitalWrite(alarmPin, LOW);
+}
+
