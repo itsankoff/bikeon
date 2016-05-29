7ba9e1ffc266b94090b22c42e222a6cd41221d04
itsankoff <itsankoff@gmail.com>
2016-05-29 20:39:50 +0300

    * bind arduino lock to whole system

--- bike_guard/lock.py
+++ bike_guard/lock.py
@@ -2,26 +2,15 @@
 # import time
 # 
 # 
-# GPIO.setmode(GPIO.BCM)
-# GPIO.setup(11, GPIO.OUT)
-# GPIO.setup(9, GPIO.IN)
-# GPIO.output(11, GPIO.LOW)
-
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(11, GPIO.OUT)
+GPIO.setup(9, GPIO.IN)
 
 def lock():
+    GPIO.output(11, GPIO.HIGH)
     return True
 
 def unlock():
+    GPIO.output(11, GPIO.LOW)
+    GPIO.cleanup()
     return True
-
-# while True:
-# 	if GPIO.input(9) == True:
-# 		print("BEIBE BEIBE")
-# 		
-# 		GPIO.output(11, GPIO.HIGH)
-# 		time.sleep(5)
-# 
-# 		GPIO.cleanup()
-# 
-# 		break
-# 
